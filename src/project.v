4c81eba3c4a76f766f54deb624b1e42e828af9a4
Michael Yu <zk.yu26@gmail.com>
2026-09-12 22:36:34 -0400

    Initial setup for onboarding

--- src/project.v
+++ src/project.v
@@ -5,7 +5,28 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_uwasic_onboarding_michael_yu (
+    assign uio_oe = 8'hFF; // Set all IOs to output
+
+    wire [7:0] en_reg_out_7_0;
+    wire [7:0] en_reg_out_15_8;
+    wire [7:0] en_reg_pwm_7_0;
+    wire [7:0] en_reg_pwm_15_8;
+    wire [7:0] pwm_duty_cycle;
+
+    pwm_peripheral pwm_peripheral_inst (
+      .clk(clk),
+      .rst_n(rst_n),
+      .en_reg_out_7_0(en_reg_out_7_0),
+      .en_reg_out_15_8(en_reg_out_15_8),
+      .en_reg_pwm_7_0(en_reg_pwm_7_0),
+      .en_reg_pwm_15_8(en_reg_pwm_15_8),
+      .pwm_duty_cycle(pwm_duty_cycle),
+      .out({uio_out, uo_out})
+    );
+
+    wire _unused = &{ena, ui_in[7:3], uio_in, 1'b0};
+    
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
